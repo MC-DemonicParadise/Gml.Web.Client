c8bcf42a74e2441d0185e70d5ae1d4c4fa944676
Nik497926 <2000-nikitos@bk.ru>
2024-07-18 20:07:23 +0300

    Update index.ts

--- src/core/configs/index.ts
+++ src/core/configs/index.ts
@@ -1,5 +1,5 @@
-export const BRAND_NAME = "Demonic";
-export const PLATFORM_SHORT_APP_NAME = "Launcher";
+export const BRAND_NAME = "GML";
+export const PLATFORM_SHORT_APP_NAME = "Frontend";
 export const PLATFORM_APP_NAME = `${BRAND_NAME} ${PLATFORM_SHORT_APP_NAME}`;
 
 type Config = {
